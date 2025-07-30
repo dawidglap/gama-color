feb27a65308aa6ca2b8fdb282b905ec4d73389e4
Dawid Glapiak <dg18@live.it>
2025-07-30 12:41:43 +0200

    apple card carousle another fix

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -124,7 +124,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
                     duration: 0.5,
                     delay: 0.2 * index,
                     ease: "easeOut",
-                    once: true,
+                    
                   },
                 }}
                 key={"card" + index}
